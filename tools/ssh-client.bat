35e216502d8a0d51308a3f7cf5b6bede2ce54465
Simeon Nedev <simeonnedev6@gmail.com>
2020-08-12 15:06:52 +0100

    modify build scripts

--- tools/ssh-client.bat
+++ tools/ssh-client.bat
@@ -1,1 +1,1 @@
-ssh -i C:\Users\Ia\Documents\SSH\LightsailDefaultKey-eu-west-2.pem -L 3000:localhost:3000 bitnami@build.processesjs.com
+ssh -i C:\Users\Ia\Documents\SSH\LightsailDefaultKey-eu-west-2.pem -L 3000:localhost:3000 bitnami@3.9.30.75
