dda85078f8765b44fa35df04ed1c4dfb848d3cc3
Simeon Nedev <simeonnedev6@gmail.com>
2020-08-13 20:43:50 +0100

    n

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -15,7 +15,7 @@ export default {
 	plugins : [
 		new HtmlWebpackPlugin({ title : 'Advance SVG' , template : './src/index.html', inject : true , chunks : ['main'] }),
     new MiniCssExtractPlugin({ filename: 'style.css' }),
-    new CopyPlugin({ patterns: [ { from: path.resolve(__dirname, 'src', '*.svg') , to: path.resolve(__dirname, 'dist' ) } ] })
+    new CopyPlugin({ patterns: [ { from: path.resolve(__dirname, '../src', '*.svg') , to: path.resolve(__dirname, '../dist' ) } ] })
 	],
 	module  : { rules: [
       { test: /\.js$/ , exclude: /node_modules/ , use: { loader: 'babel-loader' } },
