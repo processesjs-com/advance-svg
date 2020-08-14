eb506e7e111c35b546eff30f22212026b66950ef
Simeon Nedev <simeonnedev6@gmail.com>
2020-08-14 17:45:37 +0100

    edit index.html

--- src/asvg.js
+++ src/asvg.js
@@ -4,13 +4,16 @@ import './style.css'
 class ASVG{
 
   constructor(){
+
     this.displayBreakpoints = new Map([
       [ 'L', { min:1300 , max:Infinity }],
       [ 'M', { min:850  , max:1300     }],
       [ 'S', { min:350  , max:850      }],
       [ 'T', { min:0    , max:350      }]
     ])
+
     this.asvgParams = new WeakMap() // Map of all asvg divs - to be populated by searchAllASVG
+
   }
 
   onWindowLoad( event ){
