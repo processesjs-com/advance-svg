9b3be91df7eab1e9a18a8c609272b3a32e56c9f9
Simeon Nedev <simeonnedev6@gmail.com>
2020-08-15 22:11:47 +0100

    cheerio

--- src/asvg.js
+++ src/asvg.js
@@ -43,10 +43,9 @@ class ASVG{
 
 // Functions
   updateAll( ){
-    console.log( this.c$ )
-    //for(let div of this.c$.find( 'div[data-asvg]' ) ){
-     // this.updateParams( div )
-    //}
+    for(let div of this.c$( 'div[data-asvg]' ) ){
+      this.updateParams( div )
+    }
   }
 
   updateParams( div ){ console.log( div ) }
