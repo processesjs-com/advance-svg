02f7b531451c7c9d7985750ab66086c23945ea62
Simeon Nedev <simeonnedev6@gmail.com>
2020-08-16 17:09:47 +0100

    asvg

--- src/asvg.js
+++ src/asvg.js
@@ -1,55 +1,52 @@
 import 'whatwg-fetch'
-import cheerio from 'cheerio'
+import $ from 'jquery'
 import './style.css'
 
 class ASVG{
 
   constructor(){
-
     this.displayBreakpoints = new Map([
       [ 'L', { min:1300 , max:Infinity }],
       [ 'M', { min:850  , max:1300     }],
       [ 'S', { min:350  , max:850      }],
       [ 'T', { min:0    , max:350      }]
     ])
-
     this.asvgParams = new WeakMap() // Map of all asvg divs
-
-    this.config = { svgFilesFolder: '' }
-
-    let serializer = new XMLSerializer()
-    this.c$ = cheerio.load( serializer.serializeToString( document ) , { ignoreWhitespace: true, xmlMode: true } )
-
-    // Bind this to all methods
+    this.config = { svgFilesFolder: './' }
+    // Bind 'this' to all methods except the constructor
     Object.getOwnPropertyNames( Object.getPrototypeOf( this ) ).map( key => {
       if( key != 'constructor' && typeof this[key] == 'function' ){ this[key] = this[key].bind(this) }
     })
   }
 
 // Event handlers
-  onWindowLoad( event ){
-    this.updateAll()
-  }
+  onWindowLoad( event ){ this.updateAll() }
+  onWindowResize( event ){ }
 
-  onWindowResize( event ){
-
-  }
-
-  onPopupCloseClick( popupClose ){}
-
-  onPopupLinkClick( popuplink ){}
-
-  onSvgLinkClick( svglink ){}
+  onPopupCloseClick( element ){}
+  onPopupLinkClick( element ){}
+  onSvgLinkClick( element ){}
 
 // Functions
   updateAll( ){
-    console.log( this.c$( 'div[data-asvg]' ) )
-    //for(let div of this.c$( 'div[data-asvg]' ) ){
-    //  this.updateParams( div )
-    //}
+    for(let div of $( 'div[data-asvg]' ) ){ this.updateParams( div ) }
   }
 
-  updateParams( div ){ console.log( div ) }
+  updateParams( div ){
+    if( !this.asvgParams.has( div ) ){
+      this.asvgParams.set( div , {
+        root: $( div ).data( 'asvg' ) ,injected:null, currentDisplay:null, targetDisplay:null
+      })
+    }
+    let svg = this.asvgParams.get( div )
+    for(let [ label , size ] of this.displayBreakpoints.entries()){
+      if( div.offsetWidth >= size.min && div.offsetWidth < size.max ){
+        if( svg.targetDisplay != label ){ svg.targetDisplay = label }
+        break
+      }
+    }
+    console.log( svg )
+  }
 
 }
 
