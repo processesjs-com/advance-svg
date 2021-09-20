7d87095110ccaaf8d62a4783a8e82cd0c5a411d9
Simeon Nedev <simeonnedev6@gmail.com>
2021-09-20 16:39:18 +0100

    Add popuplink icon

--- tools/ssh-client.bat
+++ tools/ssh-client.bat
@@ -1,1 +1,1 @@
-ssh -i C:\Users\Ia\Documents\SSH\LightsailDefaultKey-eu-west-2.pem -L 3000:localhost:3000 bitnami@3.9.30.75
+ssh -i C:\Users\Ia\Documents\SSH\LightsailDefaultKey-eu-west-2.pem -L 80:localhost:80 bitnami@3.9.30.75
