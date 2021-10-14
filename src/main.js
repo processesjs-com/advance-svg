3dcfa3bb8fbfaad416724eb5312fc4358f053a7f
Vasil Nedev <vasilnedev55@gmail.com>
2021-10-14 17:03:26 +0100

    m

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import ASVG from './asvg'
 
-const window.asvg = new ASVG()
+window.asvg = new ASVG()
 
 window.addEventListener('load'  , asvg.updateAll )
 window.addEventListener('resize', asvg.updateAll )
