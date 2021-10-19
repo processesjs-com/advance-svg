a9dfbab21f21b0dcadffa50c6c5de6761674d7c5
Vasil Nedev <vasilnedev55@gmail.com>
2021-10-19 21:57:19 +0100

    m

--- src/js/trFilterVisio2013.js
+++ src/js/trFilterVisio2013.js
@@ -155,7 +155,7 @@ const trFilterVisio2013 = ( origSvg ) =>{
     }
 
     // Replace all ids with unique ones
-    ids.forEach( id => svgStr.replace( id , uuidv4() ) )
+    ids.forEach( id => svgStr.replaceAll( id , uuidv4() ) )
 
     // Text search - remove all tabulations, new lines and multiple spaces
     svgStr = svgStr.replace(/\t/g,' ')
