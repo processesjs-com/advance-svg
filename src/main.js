ddbe8fc8c6a3034bf49e6af59db0668a5bc9e892
Vasil Nedev <vasilnedev55@gmail.com>
2021-10-30 13:35:06 +0100

    That's it

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import ASVG from './asvg'
 
 /*
-  An instance of the ASVG class, named 'asvg', must be exposed via window object to make onclick handlers available
+  An instance of the ASVG class, named 'asvg', must be exposed via the window object to make onclick handlers available
 */
 
 if( !window.asvg ){
-  window.asvg = new ASVG() // Can pass an object with parameters: defaultFileLocation, userErrorHandler and iconMargin
+  window.asvg = new ASVG() // Accepts a parameter object with these properties: defaultFileLocation, userErrorHandler and iconMargin
   window.addEventListener('load'  , asvg.updateAll )
   window.addEventListener('resize', asvg.updateAll )
 }
