a3112365ce85cef89bf3866224ee2ffe0d0056b4
Vasil Nedev <vasilnedev55@gmail.com>
2021-11-20 20:28:29 +0000

    m

--- src/js/fitSvg.js
+++ src/js/fitSvg.js
@@ -8,6 +8,7 @@ const fitSvg = ( element , targetDisplay ) => {
       let viewBox = { x:0 , y:0 , width:100 , height:100 }
       let display = svg.querySelector('[data-asvg-display="' + targetDisplay + '"]')
       if( display ){
+        display.style.visibility='visible'
         viewBox = display.getBBox()
         /*
         let translate = getTranslateAttr( display )
@@ -25,7 +26,7 @@ const fitSvg = ( element , targetDisplay ) => {
       svg.setAttribute( 'viewBox' , ''+ viewBox.x +' '+ viewBox.y +' '+ viewBox.width +' '+ viewBox.height )
       svg.setAttribute( 'width'   , ''+ element.offsetWidth +'px' )
       svg.setAttribute( 'height'  , ''+ viewBox.height * element.offsetWidth / viewBox.width +'px' )
-
+      display.style.visibility='hidden'
       resolve ()
     }else{ reject( new Error('Could not find SVG object.') ) }
   } )
