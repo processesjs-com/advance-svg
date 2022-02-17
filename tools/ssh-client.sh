c5a131f3429878c8793d4a959846b38d657b2fc0
Vasil Nedev <vasilnedev55@gmail.com>
2022-02-17 17:29:59 +0000

    update ssh-client.sh

--- tools/ssh-client.sh
+++ tools/ssh-client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ~/.ssh/LightsailDefaultKey-eu-west-2.pem -L 3000:localhost:80 bitnami@3.9.30.75
+ssh -i ~/.ssh/LightsailDefaultKey-eu-west-2.pem -L 80:localhost:80 bitnami@3.9.30.75
